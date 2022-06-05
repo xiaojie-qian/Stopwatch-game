25bd5149d37030ded7873c03314b9f261657c36e
Summervanille <xiaojie.qian@hhl.de>
2022-06-05 18:07:04 +0200

    Add files via upload

---
+++ Fundamentals of Computing Specialization.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('hallo,World')
+
+
+# In[1]:
+
+
+print(1)
+
+
+# In[2]:
+
+
+print(int('Hallo'))
+
+
+# In[3]:
+
+
+print(type(int))
+
+
+# In[4]:
+
+
+print(-9/4)
+
+
+# In[ ]:
+
+
+
+
